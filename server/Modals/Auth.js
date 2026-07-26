33bd1e5a62273a07b32f2d6d1338ceae1772406f
Amresh <138843165+ammunirala@users.noreply.github.com>
2026-07-26 21:07:47 +0530

    Add user download plan and daily limit

--- server/Modals/Auth.js
+++ server/Modals/Auth.js
@@ -1,11 +1,35 @@
 import mongoose from "mongoose";
+
 const userschema = mongoose.Schema({
-  email: { type: String, required: true },
-  name: { type: String },
-  channelname: { type: String },
-  description: { type: String },
-  image: { type: String },
-  joinedon: { type: Date, default: Date.now },
+  email: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  channelname: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  joinedon: {
+    type: Date,
+    default: Date.now,
+  },
+  plan: {
+    type: String,
+    enum: ["free", "premium"],
+    default: "free",
+  },
+  dailyDownloadLimit: {
+    type: Number,
+    default: 1,
+  },
 });
 
-export default mongoose.model("user", userschema);
+export default mongoose.models.user || mongoose.model("user", userschema);
